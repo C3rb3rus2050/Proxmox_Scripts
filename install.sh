a79b4ea76f0d76a543fbc710b6eb655d5c3e13d0
C3rb3rus2050 <135362416+C3rb3rus2050@users.noreply.github.com>
2025-08-09 20:56:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,16 +16,16 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+apt-get update -y
 
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # install Docker-Compose
 apt install docker.io docker-compose -y
 
-apt install pip
+apt install pip -y
 
-apt install python3-setuptools
+apt install python3-setuptools -y
 
 # install net-tools
 apt install net-tools -y
